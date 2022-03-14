d330e9064b825327dd4439cb716b5c5ca0d3f54c
Mike Saraus <mizkie98@gmail.com>
2022-03-14 14:59:12 +0800

    Initial test v

---
+++ README.md
@@ -0,0 +1,41 @@
+# TPPA
+
+### Requirements
+
+- NodeJS ^v16.13.2
+- NPM ^8.4.1
+- Express Realtime API
+
+<hr>
+
+### Clone the Repository
+
+```bash
+git clone https://gitlab.com/mikesaraus/tppa_spa.git
+```
+
+### Install Node Modules
+
+```bash
+cd tppa_spa && npm install
+```
+
+### Run in Development Mode
+
+```bash
+npm run dev
+```
+
+### Run in Production Mode
+
+```bash
+npm start
+```
+
+<hr>
+
+### Essential Links
+
+Download [NodeJS](https://nodejs.dev/download/).
+
+Express Realtime API [expsql_rtapi](https://gitlab.com/mikesaraus/expsql_rtapi.git)
---
+++ app.js
@@ -0,0 +1,84 @@
+'use strict'
+
+require('dotenv').config()
+
+const _ = process.env,
+  express = require('express'),
+  app = express(),
+  port = _.PORT || 8088
+
+const compression = require('compression'),
+  bodyParser = require('body-parser'),
+  morgan = require('morgan'),
+  cron = require('node-cron')
+
+const { throttle, decodeURL } = require('./lib/middleware'),
+  { readFileSync, existsSync } = require('fs'),
+  CGU = require('cron-updater')
+
+const newUpdater = new CGU({
+  repository: 'https://gitlab.com/mikesaraus/expsql_rtapi.git',
+  branch: 'development',
+  fromReleases: false,
+  tempLocation: '../history',
+  ignoreFiles: ['.env'],
+  exitOnComplete: false,
+})
+// newUpdater.update()
+// newUpdater.forceUpdate()
+
+// Some Process
+process.title = _.npm_package_name || process.title
+process.env.TZ = _.TZ || 'Asia/Manila'
+
+// Some Middlewares
+app
+  .use(compression())
+  .use(bodyParser.urlencoded({ extended: true }))
+  .use(decodeURL()) // Check URL
+  .use(throttle(5 * 1024 * 1024)) // Throttling bandwidth (bytes)
+  .disable('x-powered-by')
+
+// Log Requests
+morgan.token('datetime', (_req, res) => new Date())
+app.use(
+  morgan(
+    `[:datetime] :remote-addr - :remote-user ":method :url HTTP/:http-version" (:response-time ms) :status :res[content-length] ":referrer" ":user-agent" (Total :total-time ms)`
+  )
+)
+
+// Check if History Mode
+if (_.MODE == 'history') {
+  const history = require('connect-history-api-fallback')
+  app.use(history())
+}
+
+// Create HTTP or HTTPS Server
+const createServer = () => {
+  const key = _.SSL_KEY
+  const cert = _.SSL_CERT
+  const ssl =
+    existsSync(key) && existsSync(cert)
+      ? {
+          key: readFileSync(key),
+          cert: readFileSync(cert),
+        }
+      : null
+  return ssl
+    ? // https://
+      require('https').createServer(ssl, app)
+    : // http://
+      require('http').createServer(app)
+}
+
+// Initialize Server
+const server = createServer()
+
+// SPA - Single Page Application
+app.use(express.static('./app'))
+
+server.listen(port, () => {
+  console.log('#'.repeat(50))
+  console.info(`Server is up and running on *:${port}`)
+  console.log('#'.repeat(50))
+})
